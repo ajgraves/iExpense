5263def3b5b67160f8fb753c069417eb845ab1ec
Aaron Graves <ajgraves@ajgraves.com>
2024-07-21 23:01:19 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Aaron Graves on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
