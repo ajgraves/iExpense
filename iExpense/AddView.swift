b14946af671407a9912397fe3212b01f66182d33
Aaron Graves <ajgraves@ajgraves.com>
2024-07-22 16:21:37 -0500

    Completed day 2 work

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Aaron Graves on 7/22/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
